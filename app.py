7f55cf9e961d07103ec01e6516a59b612e05e69c
johnma <ma.jonathan02@gmail.com>
2023-10-21 12:25:38 -0400

    feat: add second model

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flasgger import Swagger
 from datetime import datetime
 
 crime_model = None
+crash_model = None
 
 
 app = Flask(__name__)
@@ -17,6 +18,7 @@ def hello():
     return "Hello World!"
 
 
+# https://www.kaggle.com/code/jonathanma02/hack-harvard-crime-model/edit
 @app.route('/predict_crime', methods=['POST'])
 def predict_crime():
     """Endpoint returning a classification and regression value for crime prediction based on location and time data.
@@ -79,6 +81,76 @@ def predict_crime():
             'month_cos': [np.cos(2 * np.pi * date.month / 12.0)],
         })
         prediction = crime_model.predict(prediction_input)
+    threshholds = [0, 1, 2, 3, 4, 5]
+    return jsonify({
+        "prediction": np.digitize(prediction, threshholds).tolist()[0],
+        "regression": prediction.tolist()[0]},
+        200)
+
+
+#https://www.kaggle.com/jonathanma02/hack-harvard-crash-model
+@app.route('/predict_crash', methods=['POST'])
+def predict_crash():
+    """Endpoint returning a classification and regression value for crash prediction based on location and time data.
+    ---
+    parameters:
+      - in: body
+        name: input_data
+        description: JSON input data for prediction.
+        required: true
+        schema:
+          type: object
+          properties:
+            lat:
+              type: number
+              format: float
+              description: Latitude.
+            lng:
+              type: number
+              format: float
+              description: Longitude.
+            date:
+              type: string
+              format: date-time
+              description: Date and time.
+
+    responses:
+      200:
+        description: Classification and regression values for crash prediction based on location and time data.
+        schema:
+          type: object
+          properties:
+            prediction:
+              type: number
+              description: Classification value.
+            regression:
+              type: number
+              format: float
+              description: Regression value.
+        examples:
+          application/json:
+            {
+              "prediction": 3,
+              "regression": 3.14159
+            }
+    """
+    data = request.get_json()
+    lat = data.get('lat')
+    lng = data.get('lng')
+    date = datetime.fromisoformat(data.get('date'))
+    with open('./models/crash_model_OSVM.pkl', 'rb') as model_file:
+        crash_model = pickle.load(model_file)
+    prediction = None
+    if crash_model is not None:
+        prediction_input = pd.DataFrame({
+            'lat': [lat],
+            'long': [lng],
+            'hour_sin': [np.sin(2 * np.pi * date.hour / 24.0)],
+            'hour_cos': [np.cos(2 * np.pi * date.hour / 24.0)],
+            'month_sin': [np.sin(2 * np.pi * date.month / 12.0)],
+            'month_cos': [np.cos(2 * np.pi * date.month / 12.0)],
+        })
+        prediction = crash_model.predict(prediction_input)
     threshholds = [0, 1, 2, 3, 4, 5]
     return jsonify({
         "prediction": np.digitize(prediction, threshholds).tolist()[0],
